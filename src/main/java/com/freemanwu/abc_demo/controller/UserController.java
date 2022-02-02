d50272e7b55a4015cee45409d247ff9bbdb1b636
searchingwu <searchingwu@163.com>
2022-02-02 21:19:48 +0800

    修改了部分页面跳转的bug，完善了用户部分的系统功能，待曲谱功能接入。 2021-02-02 21：20

--- src/main/java/com/freemanwu/abc_demo/controller/UserController.java
+++ src/main/java/com/freemanwu/abc_demo/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 @Controller
 @RequestMapping("user")
@@ -15,32 +16,72 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 用户注册
+     * @param user
+     * @return
+     */
     @RequestMapping("register")
     public String register(User user){
         userService.register(user);
         return "Welcome";
     }
 
+    /**
+     * 用户登陆
+     * @param user
+     * @param session
+     * @param model
+     * @return
+     */
     @RequestMapping("findByNameAndPassword")
     public String findByNameAndPassword(User user, HttpSession session, Model model){
        User loginUser = userService.findByNameAndPassword(user);
        if (loginUser != null){
            session.setAttribute("loginUser",loginUser);
-//           model.addAttribute("loginUser",loginUser);
+//           model.addAttribute("User",loginUser);
            return "First";
        }else
            return "Error";
     }
 
+    /**
+     * 用户主页
+     * @param user
+     * @return
+     */
     @RequestMapping("findByName")
-    public String findByName(User user){
-        userService.findByName(user);
+    public String findByName(User user,Model model){
+        User UserInfo = userService.findByName(user);
+        model.addAttribute("Model_User_Info",UserInfo);
         return "UserHomePage";
     }
 
+    /**
+     * 用户返回主页重定向
+     * @param user
+     * @return
+     */
     @RequestMapping("First_Redirect")
-    public String First_Redirect(User user){
-        userService.findByName(user);
+    public String First_Redirect(User user,Model model){
+        User UserInfo = userService.findByName(user);
+        model.addAttribute("Model_User_Info",UserInfo);
         return "First";
+    }
+
+    //用户信息更新
+    @RequestMapping("update1")
+    public String update1(User user, Model model){
+        User UserInfo = userService.findByName(user);
+        model.addAttribute("Model_User_Info", UserInfo);
+        return "UserChange";
+    }
+
+    @RequestMapping("update2")
+    public String update2(User user, Model model){
+        userService.update(user);
+        User UserInfo = userService.findByName(user);
+        model.addAttribute("Model_User_Info",UserInfo);
+        return "UserHomePage";
     }
 }
